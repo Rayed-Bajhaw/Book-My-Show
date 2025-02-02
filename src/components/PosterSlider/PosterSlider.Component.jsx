d0508605933fe8876b846b7567f0994034f7e521
Rayed-Bajhaw <bajhawrayed77@gmail.com>
2025-02-02 22:43:44 +0530

    Day-07-MoviePageCastAndCrew

--- src/components/PosterSlider/PosterSlider.Component.jsx
+++ src/components/PosterSlider/PosterSlider.Component.jsx
@@ -1,56 +1,3 @@
-// import React from "react";
-// import Slider from "react-slick";
-// import Poster from "../Poster/Poster.Component";
-
-// const PosterSlider = (props) => {
-//   const { posters, title, subtitle, isDark, config } = props;
-//   const settings = {
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 5,
-//     slidesToScroll: 4,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 2,
-//         },
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//         },
-//       },
-//     ],
-//   };
-//   return (
-//     <>
-//       <div className="flex flex-col items-start sm:ml-3 my-2">
-//         <h3
-//           className={`text-2xl font-bold ${
-//             isDark ? "text-white" : "text-black"
-//           } `}
-//         >
-//           {title}
-//         </h3>
-//         <p className={`text-sm ${isDark ? "text-white" : "text-gray-800"} `}>
-//           {subtitle}
-//         </p>
-//       </div>
-//       <Slider {...settings}>
-//         {posters.map((each, index) => (
-//           <Poster {...each} isDark={isDark} key={index} />
-//         ))}
-//       </Slider>
-//     </>
-//   );
-// };
-
-// export default PosterSlider;
-
 import React from "react";
 import Slider from "react-slick";
 import Poster from "../Poster/Poster.Component";
@@ -85,19 +32,28 @@ const PosterSlider = (props) => {
         <h3
           className={`text-2xl font-bold ${
             isDark ? "text-white" : "text-black"
-          }`}
+          } `}
         >
           {title}
         </h3>
-        <p className={`text-sm  ${isDark ? "text-white" : "text-gray-800"}`}>
+        <p className={`text-sm ${isDark ? "text-white" : "text-gray-800"} `}>
           {subtitle}
         </p>
       </div>
-      <Slider {...settings}>
-        {posters.map((each, index) => (
-          <Poster {...each} isDark={isDark} key={index} />
-        ))}
-      </Slider>
+      {config && ( // if we have config we will go with config settings which is in moviePage
+        <Slider {...config}>
+          {posters.map((each, index) => (
+            <Poster {...each} isDark={isDark} key={index} />
+          ))}
+        </Slider>
+      )}
+      {!config && (
+        <Slider {...settings}>
+          {posters.map((each, index) => (
+            <Poster {...each} isDark={isDark} key={index} />
+          ))}
+        </Slider>
+      )}
     </>
   );
 };
