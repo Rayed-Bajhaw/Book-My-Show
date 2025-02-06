559bb9c67f733820b1acaa00477b402ca47060f2
Rayed-Bajhaw <bajhawrayed77@gmail.com>
2025-02-06 09:46:15 +0530

    Day-09-PlayPage

--- src/pages/Play.Page.jsx
+++ src/pages/Play.Page.jsx
@@ -1,8 +1,86 @@
 import React from "react";
 import DefaultLayoutHoc from "../layout/Default.layout";
 
+// components
+import Poster from "../components/Poster/Poster.Component";
+import PlayFilters from "../components/PlayFilters/PlayFilters.Component";
+
 const PlayPage = () => {
-  return <div>PlayPage</div>;
+  return (
+    <>
+      <div className="container mx-auto px-4 my-10">
+        <div className="w-full flex flex-col-reverse lg:flex-row-reverse gap-4">
+          <h2 className="text-2xl font-bold mb-4">Plays in Hospet</h2>
+          <div className="flex flex-wrap">
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+            <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
+              <Poster
+                isPlay={true}
+                src=""
+                title="So Rude of Me By Sweets"
+                subtitle="Comedy Shows | English, Kannada | 18yrs+ | 2hr 30mins"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="lg:w-1/4 p-4 bg-gray-100 rounded">
+          <h2 className="text-2xl font-bold mb-4">Filters</h2>
+          <div className="">
+            <PlayFilters
+              title="Date"
+              tags={["Today", "Tomorrow", "This weekend"]}
+            />
+          </div>
+          <div className="">
+            <PlayFilters
+              title="Language"
+              tags={["English", "Kannada", "Hindi", "Telugu"]}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DefaultLayoutHoc(PlayPage);
